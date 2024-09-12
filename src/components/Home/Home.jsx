6d2d6f34bac04168d740e607283538f57b48af64
connorrewa <connorrewa13@hotmail.com>
2024-09-11 21:57:34 -0400

    feat: add navbar to home and shop pages"

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,20 @@
 import { Link } from 'react-router-dom';
 import Navbar from '../Navbar/Navbar';
+import styles from './Home.module.css';
 
 const Home = () => {
     return (
         <>
             <Navbar />
-            <h1>Home</h1>
+            <div className='container'>
+                <div className='content'>
+                    <h2>Lema</h2>
+                    <p>
+                        The marketplace with the best deals <span>for you</span>
+                    </p>
+                </div>
+                <Link to='/shop'>Shop Now</Link>
+            </div>
         </>
     );
 };
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,26 @@
 import { Link } from 'react-router-dom';
-const Navbar = () => {
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import styles from './Navbar.module.css';
+import { CardTravel } from '@mui/icons-material';
+const Navbar = ({ cart, setCartIsOpen }) => {
     return (
-        <div>
-            <h1>Lima</h1>
-            <Link to='/'>Home</Link>
-            <Link to='/shop'>Shop</Link>
+        <div className={styles.container}>
+            <Link className={styles.title}>
+                <h1>Lima</h1>
+            </Link>
+            <Link className={styles.link} to='/'>
+                Home
+            </Link>
+            <Link className={styles.link} to='/shop'>
+                Shop
+            </Link>
+            <button onClick={() => setCartIsOpen(true)}>
+                {cart.length !== 0 && (
+                    <div className={styles.cartCount}>{cart.length}</div>
+                )}
+
+                <ShoppingCartIcon></ShoppingCartIcon>
+            </button>
         </div>
     );
 };
